a218a52f16bc825cff1cdbe8f78d624e09b0d272
Aditya Panda <pandaaditya48@gmail.com>
2023-06-08 11:27:03 -0500

    style

--- src/jsx/ppro/ppro.ts
+++ src/jsx/ppro/ppro.ts
@@ -1,31 +1,38 @@
-
 export const getFilePath = () => {
   const filePath = app.project.path;
   return filePath;
 };
 
-export const insertVideoDownload = (videoPath: string, toggleOverwrite : boolean, toggleNoInject : boolean, 
-                                    toggleTopTrack : boolean, toggleAudioOnly : boolean) => {
-
+export const insertVideoDownload = (
+  videoPath: string,
+  toggleOverwrite: boolean,
+  toggleNoInject: boolean,
+  toggleTopTrack: boolean,
+  toggleAudioOnly: boolean
+) => {
   // Get the active sequence
   var activeSequence = app.project.activeSequence;
   var projectItems = app.project.rootItem.children;
   // Import the video file
-  var importedClip = app.project.importFiles([videoPath], true, app.project.rootItem, false);
-  if(toggleNoInject){
+  var importedClip = app.project.importFiles(
+    [videoPath],
+    true,
+    app.project.rootItem,
+    false
+  );
+  if (toggleNoInject) {
     return;
   }
   var tracksList = activeSequence.videoTracks;
-  if(toggleAudioOnly){
+  if (toggleAudioOnly) {
     tracksList = activeSequence.audioTracks;
   }
 
   var trackToInsert = tracksList[tracksList.numTracks - 1];
-  
 
-  if(!toggleTopTrack){
-    for(var i = 0; i < tracksList.numTracks; i++){
-      if (tracksList[i].isTargeted()){
+  if (!toggleTopTrack) {
+    for (var i = 0; i < tracksList.numTracks; i++) {
+      if (tracksList[i].isTargeted()) {
         trackToInsert = tracksList[i];
         break;
       }
@@ -33,11 +40,16 @@ export const insertVideoDownload = (videoPath: string, toggleOverwrite : boolean
   }
 
   // Add the imported video to the active sequence
-  if(toggleOverwrite){
-    trackToInsert.overwriteClip(projectItems[projectItems.numItems - 1], activeSequence.getPlayerPosition()); // Insert the clip at the start of the sequence
+  if (toggleOverwrite) {
+    trackToInsert.overwriteClip(
+      projectItems[projectItems.numItems - 1],
+      activeSequence.getPlayerPosition()
+    ); // Insert the clip at the start of the sequence
   } else {
-    //@ts-ignore
-    trackToInsert.insertClip(projectItems[projectItems.numItems - 1], activeSequence.getPlayerPosition()); // Insert the clip at the start of the sequence
+    trackToInsert.insertClip(
+      projectItems[projectItems.numItems - 1],
+      // @ts-ignore
+      activeSequence.getPlayerPosition()
+    ); // Insert the clip at the start of the sequence
   }
-
 };
